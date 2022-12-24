d3ab64504d2864a83d26fa3b631cf9eef0a89ece
Victor <victor.giorgi8@gmail.com>
2022-12-24 11:49:47 -0300

    Estrutura de pastas ajustadas e removido de dados nao utilizados

---
+++ app.py
@@ -0,0 +1,16 @@
+import pandas as pd 
+import numpy as np
+import streamlit as st
+
+def load_data():
+    return pd.read_csv(r'C:/Users/victo/.repos/curso_git/projeto\data/processed/bikes_completed.csv')
+
+
+def main(): 
+    df = load_data()
+
+    st.dataframe(df)
+
+if __name__ == '__main__':
+    main()
+
